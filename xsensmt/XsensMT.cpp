b9c2369ce63fe5028c7d1840fbe55bb6a4e699e6
Silvio Traversaro <pegua1@gmail.com>
2017-12-28 11:17:11 +0100

    Avoid segmentation faults
    See https://github.com/robotology-playground/xsensmt-yarp-driver/issues/3

--- xsensmt/XsensMT.cpp
+++ xsensmt/XsensMT.cpp
@@ -231,19 +231,33 @@ void XsensMT::sensorReadLoop()
             m_sensorBuffer[2]  = euler.yaw(); //yaw
 
             // Accelerometers are expressed in m/s^2 in both YARP and in the Xsens interface
-            m_sensorBuffer[3]  = acc[0]; //accel-X
-            m_sensorBuffer[4]  = acc[1]; //accel-Y
-            m_sensorBuffer[5]  = acc[2]; //accel-Z
+            if (acc.size() == 3) {
+                m_sensorBuffer[3]  = acc[0]; //accel-X
+                m_sensorBuffer[4]  = acc[1]; //accel-Y
+                m_sensorBuffer[5]  = acc[2]; //accel-Z
+            } else {
+              yWarning() << "xsensmt: Malformed accelerometer message, skipping accelerometer update.";
+            }
             
             // Gyroscope data are expressed in rad/s in the Xsens interface, so they have to be converted in deg/s
-            m_sensorBuffer[6]  = gyro[0]*CTRL_RAD2DEG;  //gyro-X
-            m_sensorBuffer[7]  = gyro[1]*CTRL_RAD2DEG;  //gyro-Y
-            m_sensorBuffer[8]  = gyro[2]*CTRL_RAD2DEG;  //gyro-Z
+            if (gyro.size() == 3) {
+                m_sensorBuffer[6]  = gyro[0]*CTRL_RAD2DEG;  //gyro-X
+                m_sensorBuffer[7]  = gyro[1]*CTRL_RAD2DEG;  //gyro-Y
+                m_sensorBuffer[8]  = gyro[2]*CTRL_RAD2DEG;  //gyro-Z
+            } else {
+                yWarning() << "xsensmt: Malformed gyroscope message, skipping gyroscope update.";
+            }
             
             // Magnetometers 
-            m_sensorBuffer[9]  = mag[0];  //magn-X
-            m_sensorBuffer[10] = mag[1];  //magn-Y
-            m_sensorBuffer[11] = mag[2];  //magn-Z
+            if (mag.size() == 3) {
+                m_sensorBuffer[9]  = mag[0];  //magn-X
+                m_sensorBuffer[10] = mag[1];  //magn-Y
+                m_sensorBuffer[11] = mag[2];  //magn-Z
+            } else {
+                yWarning() << "xsensmt: Malformed magnetometer message, skipping magnetometer update.";
+            }
+
+            // TODO(traversaro): update sensor available logic
             m_isSensorMeasurementAvailable = true;
             m_lastStamp.update(yarp::os::SystemClock::nowSystem());
             m_bufferMutex.unlock();
