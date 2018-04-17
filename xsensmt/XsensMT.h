a156f71578097f4105f0b5e9b527b2163da3f2f3
gabrielenava <gabriele.nava@mail.polimi.it>
2018-04-17 18:48:41 +0200

    Add setting of filter profile and option flags
    For the time being, this options are hardcoded to the values
    discussed in https://github.com/robotology-playground/xsensmt-yarp-driver/issues/7 .
    If in the future it will be needed to support different configuration
    files, it would be nice to support specifing this values through the configuration
    file of the YARP device.

--- xsensmt/XsensMT.h
+++ xsensmt/XsensMT.h
@@ -10,20 +10,20 @@
 
 #include "deviceclass.h"
 
-
 #include <yarp/dev/DeviceDriver.h>
 #include <yarp/dev/GenericSensorInterfaces.h>
 #include <yarp/os/Stamp.h>
 #include <yarp/dev/PreciselyTimed.h>
 
+#include <cstdint>
 #include <atomic>
 #include <mutex>
 #include <string>
 #include <thread>
 
-namespace yarp 
+namespace yarp
 {
-namespace dev 
+namespace dev
 {
     class XsensMT;
 }
@@ -31,60 +31,65 @@ namespace dev
 
 
 /**
- * 
+ *
 * \section xsensmt Description of input parameters
 * \brief Device that implements IGenericSensor for Xsens MT* devices using the Xsens MT software suite.
 *
-* This device is based on the self-contained src_cpp example from the Xsens MT software suite. 
+* This device is based on the self-contained src_cpp example from the Xsens MT software suite.
 *
 * Parameters accepted in the config argument of the open method:
 * | Parameter name | Type    | Units | Default Value | Required  | Description   | Notes |
 * |:--------------:|:-------:|:-----:|:-------------:|:--------: |:-------------:|:-----:|
 * | serial         | string  |       | /dev/ttyUSB0  | No        | File name of the serial device to which to connect.  | |
-* | baud           | int     |       | 115200        | No        | Baud rate used by the serial communication." | | 
+* | baud           | int     |       | 115200        | No        | Baud rate used by the serial communication." | |
 * | timeout        | double  |seconds| 0.1           | No        | If the device is not receiving any sensor measure for timeout seconds, it will start reporting an error. | - |
 *
 **/
-class yarp::dev::XsensMT : public yarp::dev::IGenericSensor, 
-                           public yarp::dev::IPreciselyTimed, 
+class yarp::dev::XsensMT : public yarp::dev::IGenericSensor,
+                           public yarp::dev::IPreciselyTimed,
                            public yarp::dev::DeviceDriver
 {
 public:
     XsensMT();
     ~XsensMT();
-    
+
     // IGenericSensor interface.
     virtual bool read(yarp::sig::Vector &out);
     virtual bool getChannels(int *nc);
     virtual bool open(yarp::os::Searchable &config);
     virtual bool calibrate(int ch, double v);
     virtual bool close();
-    
+
     // Function executed by m_sensorThread
     void sensorReadLoop();
 
     virtual yarp::os::Stamp getLastInputStamp();
-    
-    // Thread running the sensorReadLoop method 
+
+    // Thread running the sensorReadLoop method
     std::thread m_sensorThread;
 
 private:
+    // Send a SetFilterProfile message to set the filter profile
+    bool setFilterProfile(const uint16_t profile);
+
+    // Send a SetOptionFlags message to set the option flags
+    bool setOptionFlags(const uint32_t setFlags, const uint32_t clearFlags);
+
     int m_nchannels{12};
     double m_timeoutInSecond{0.1};
-    
-    // True if the close method has been called at least once 
+
+    // True if the close method has been called at least once
     std::atomic<bool> m_isDeviceClosing{false};
-    
-    // True if a valid sensor measurement has been received in the last m_sensorTimeout seconds 
+
+    // True if a valid sensor measurement has been received in the last m_sensorTimeout seconds
     bool m_isSensorMeasurementAvailable{false};
-    
+
     // Interface exposed by the Xsens MT Software suite
     DeviceClass m_xsensDevice;
     XsPortInfo m_portInfo;
-   
+
     yarp::os::Stamp  m_lastReadStamp;
 
-    
     // Mutex protecting m_sensorBuffer, m_lastStamp
     std::mutex m_bufferMutex;
     yarp::sig::Vector m_sensorBuffer;
