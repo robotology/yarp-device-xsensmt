88485ced540e155788e8b138cdea1a02ad9849a7
Hosameldin Mohamed <45564317+HosameldinMohamed@users.noreply.github.com>
2022-09-28 12:09:40 +0200

    Add parameter period to set the output frequency of XSens device (#34)
    * add param period
    
    * change fine to asString
    
    * test which data type is found
    
    * period check isInt or isFloat
    
    * set period in seconds + add small description

--- xsensmt/XsensMT.h
+++ xsensmt/XsensMT.h
@@ -46,6 +46,7 @@ namespace dev
 * | timeout        | double  |seconds| 0.1           | No        | If the device is not receiving any sensor measure for timeout seconds, it will start reporting an error. | - |
 * | sensor_name    | string  |       | sensor_imu_xsensmt       | No        | Name of the inertial sensor device. | |
 * | frame_name     | string  |       | set same as `sensor_name` | No    | Sensor frame in which the measurements are expressed. |
+* | xsensmt_period     | double  |    seconds   | 0.01  | No    | Period of querying the Xsens MT* device | The frequency of publishing the information is determined by the device that attaches this one |
 **/
 class yarp::dev::XsensMT : public yarp::dev::IGenericSensor,
                            public yarp::dev::IPreciselyTimed,
@@ -268,6 +269,8 @@ private:
     
     std::string m_sensorName;
     std::string m_frameName;
+    double m_outputPeriod;
+    double m_outputFrequency;
     
     // Send a SetFilterProfile message to set the filter profile
     bool setFilterProfile(const uint16_t profile);
